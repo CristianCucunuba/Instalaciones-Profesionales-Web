575fabaa2d587b54df2f4aea0089331bbdd9ca28
Cristian Cucunuba <cccucunuba67@ucatolica.edu.co>
2018-06-24 10:12:38 -0500

    Carousel of providers

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(document).ready( function () {
-    $(".owl-carousel").owlCarousel();
+    let owl = $('.owl-carousel')
+    owl.owlCarousel({
+        items: 5,
+        loop: true,
+        margin: 50,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoHeight: true,
+        center: true,
+        autoplayHoverPause: true
+    });
+    
 });
